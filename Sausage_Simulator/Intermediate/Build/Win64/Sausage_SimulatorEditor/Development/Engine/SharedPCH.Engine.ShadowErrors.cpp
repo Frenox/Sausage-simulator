63f6fe3ec772d04211b44b17daebe3c5475310a4
Frenox <maxime.huet15@gmail.com>
2022-03-02 12:01:58 +0100

    Removed the project to solve the problems

--- Sausage_Simulator/Intermediate/Build/Win64/Sausage_SimulatorEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.28.29910
-#include "C:/Users/maxim/Desktop/Sausage Simulator/Sausage-simulator/Sausage_Simulator/Intermediate/Build/Win64/Sausage_SimulatorEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- Sausage_Simulator/Source/Sausage_Simulator/Sausage_Simulator.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Sausage_Simulator.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Sausage_Simulator, "Sausage_Simulator" );
